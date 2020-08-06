fix luminance computation

generator/util/image.py
@@ -17,7 +17,7 @@ def lin_to_srgb(rgba):
         np.subtract(np.multiply(1.055, np.power(rgba, 1.0 / 2.4)), 0.055)), 0.0, 1.0)
 
 def luminance(img):
-    return 0.2126*img[:,:,0] + 0.7152**img[:,:,1] + 0.0722**img[:,:,2]
+    return 0.2126*img[:,:,0] + 0.7152*img[:,:,1] + 0.0722*img[:,:,2]
 
 def exposure(img, exposure):
     return img * pow(2, exposure)
@@ -59,11 +59,8 @@ def squared_error(img, ref):
 def relative_squared_error(img, ref, epsilon=0.0001):
     return (img - ref)**2 / (ref**2 + epsilon)
 
-def mean(img):
-    return np.mean(img)
-
 def mse(img, ref):
-    return mean(luminance(squared_error(img, ref)))
+    return np.mean(luminance(squared_error(img, ref)))
 
 def remove_outliers(error_img):
     f = 0.0001