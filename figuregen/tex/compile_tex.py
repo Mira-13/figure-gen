0595f7a9f641a27affafa683d4eb198ad2a74da8
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2020-10-04 19:32:36 +0200

    refactor

--- figuregen/tex/compile_tex.py
+++ figuregen/tex/compile_tex.py
@@ -1,47 +1,29 @@
 import subprocess
 import os
-from shutil import rmtree
 from shutil import copy
 
 def copy_pdf_file(tmp_folder_path, to_path, tex_filename, pdf_filename):
-    old_pathfile = os.path.join(tmp_folder_path, tex_filename.replace('.tex', '.pdf'))
-    new_pathfile = os.path.join(to_path, pdf_filename)
+    old_filename = os.path.join(tmp_folder_path, tex_filename.replace('.tex', '.pdf'))
+    new_filename = os.path.join(to_path, pdf_filename)
 
     # if to_path has already a pdf file, delete it, so it can be overwritten
     try:
-        os.remove(new_pathfile)
+        os.remove(new_filename)
     except:
         pass
 
     # get file and copy into other path
-    copy(old_pathfile, new_pathfile)
+    copy(old_filename, new_filename)
 
-def wslpath(path):
-    return subprocess.check_output(['wsl', 'wslpath', '-a', path.replace('\\','\\\\')]).decode('utf8')[:-1]
+def call_pdflatex(tmp_path, filename):
+    subprocess.check_call(['pdflatex', '-interaction=nonstopmode', filename], cwd=tmp_path, stdout=subprocess.DEVNULL)
 
-def change_includegraphics_paths_based_on_os(pathfile):
-    with open(pathfile, 'r') as f:
-        content = f.read()
-
-    import re
-    content = re.sub(r'\\includegraphics\[([^\}]*)\]\{([^\}]*)\}', lambda match: '\\includegraphics[' + match.groups()[0] + ']{' + wslpath(match.groups()[1]) + '}', content) # braces (}) in filename not supported!
-
-    with open(pathfile, 'w') as f:
-        f.write(content)
-
-def call_pdflatex(is_os_windows, is_latex_on_linux, tmp_path, filename):
-    try:
-        subprocess.check_call(['pdflatex', '-interaction=nonstopmode', filename], cwd=tmp_path,
-            stdout=subprocess.DEVNULL) # TODO make this an option: piping to DEVNULL suppresses all LaTeX output
-    except subprocess.CalledProcessError:
-        pass # TODO handle pdflatex errors
-
-def compile(module_path, tex_filename, pdf_filename, is_os_windows=True, is_latex_on_linux=False):
+def compile(module_path, tex_filename, pdf_filename):
     '''
         Compiles a .tex file to .pdf
     arg 'pdf_filename' needs to include '.pdf'-ending
     Generates a temporary folder, where pdflatex will compile the output in in addition with it's default other files,
     copies only the generated pdf file into the same path where the .tex file lies and delete the tmp folder afterwards.
     '''
-    call_pdflatex(is_os_windows, is_latex_on_linux, module_path, tex_filename)
+    call_pdflatex(module_path, tex_filename)
     copy_pdf_file(module_path, module_path, tex_filename, pdf_filename)
