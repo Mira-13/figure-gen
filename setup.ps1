95c3c9e0ee4f48d0b97af14742a6a59815e4ea93
Mira Niemann <mira.niemann@gmail.com>
2020-11-19 15:55:13 +0100

    v0.6: plots in grids

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
 python -m pip install wheel
 python setup.py sdist bdist_wheel
 python -m pip uninstall figuregen
-python -m pip install --upgrade .\dist\figuregen-0.5.0-py3-none-any.whl
+python -m pip install --upgrade .\dist\figuregen-0.6.0-py3-none-any.whl
