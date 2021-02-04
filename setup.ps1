1d931431f6624f492ea08c38360a6611f16aafa2
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2021-02-04 14:11:33 +0100

    v0.7.1

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
 python -m pip install wheel
 python setup.py sdist bdist_wheel
 python -m pip uninstall figuregen
-python -m pip install --upgrade .\dist\figuregen-0.7.0-py3-none-any.whl
+python -m pip install --upgrade .\dist\figuregen-0.7.1-py3-none-any.whl
