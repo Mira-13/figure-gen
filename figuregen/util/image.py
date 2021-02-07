a41d6097a4c14b3af271f1e8272b97ffe22bdc5b
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2021-02-07 14:40:49 +0100

    use faster util from simpleimageio

--- figuregen/util/image.py
+++ figuregen/util/image.py
@@ -1,32 +1,11 @@
 import numpy as np
 import scipy.ndimage
 
+from simpleimageio import lin_to_srgb, luminance, exposure, average_color_channels, zoom
+
 class Error(Exception):
     def __init__(self, message):
         self.message = message
-
-def lin_to_srgb(rgba):
-    """
-    Linear to sRGB conversion of n-d numpy array:
-    rgb<=0.0031308?rgb*12.92:(1.055*(rgb**(1.0/2.4)))-0.055
-    """
-    return np.clip(np.where(
-        np.less_equal(rgba, 0.0031308),
-        np.multiply(rgba,  12.92),
-        np.subtract(np.multiply(1.055, np.power(rgba, 1.0 / 2.4)), 0.055)), 0.0, 1.0)
-
-def luminance(img):
-    return 0.2126*img[:,:,0] + 0.7152*img[:,:,1] + 0.0722*img[:,:,2]
-
-def exposure(img, exposure):
-    return img * pow(2, exposure)
-
-def average_color_channels(img):
-    assert(img.shape[2] == 3)
-    return np.sum(img, axis=2) / 3.0
-
-def zoom(img, scale=20):
-    return scipy.ndimage.zoom(img, (scale, scale, 1), order=0)
 
 def crop(img, crop_args):
     '''
@@ -220,30 +199,13 @@ class SplitImage:
     def get_weights(self):
         return self.weights
 
+from simpleimageio import mse, relative_mse, relative_mse_outlier_rejection
+
 def squared_error(img, ref):
     return (img - ref)**2
 
 def relative_squared_error(img, ref, epsilon=0.0001):
     return (img - ref)**2 / (ref**2 + epsilon)
-
-def mse(img, ref):
-    return np.mean(luminance(squared_error(img, ref)))
-
-def remove_outliers(error_img, percentage):
-    errors = np.sort(error_img.flatten())
-    num_outliers = int(errors.size * 0.01 * percentage)
-    e = errors[0:errors.size-num_outliers]
-    return np.mean(e)
-
-def relative_mse(img, ref, epsilon=0.0001):
-    err_img_rgb = relative_squared_error(img, ref, epsilon)
-    err_img_gray = average_color_channels(err_img_rgb)
-    return np.mean(err_img_gray)
-
-def relative_mse_outlier_rejection(img, ref, epsilon=0.0001, percentage=0.1):
-    err_img_rgb = relative_squared_error(img, ref, epsilon)
-    err_img_gray = average_color_channels(err_img_rgb)
-    return remove_outliers(err_img_gray, percentage)
 
 def sape(img, ref):
     ''' Computes the symmetric absolute precentage error
