ecbd5b603e70624ef66c48ba415f3d2669449eb8
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2024-01-17 17:26:16 +0100

    v1.1.0

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ python -m pip install build
 rm -rf ./build
 rm -rf ./dist
 python -m build
-python -m pip install --upgrade ./dist/figuregen-1.0.1-py3-none-any.whl
+python -m pip install --upgrade ./dist/figuregen-1.1.0-py3-none-any.whl
