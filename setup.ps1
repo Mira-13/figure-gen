befe1f9f5f2525a0a6329ff099b322a40a1d533b
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2025-06-13 18:50:00 +0200

    v1.3.0

--- setup.ps1
+++ setup.ps1
@@ -2,4 +2,4 @@ python -m pip install build
 rm -Recurse -Force .\build
 rm -Recurse -Force .\dist
 python -m build
-python -m pip install --upgrade .\dist\figuregen-1.2.0.tar.gz
+python -m pip install --upgrade .\dist\figuregen-1.3.0.tar.gz
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='figuregen',
-    version='1.2.0',
+    version='1.3.0',
     description='Figure Generator',
     long_description=long_description,
     long_description_content_type="text/markdown",
